f12617cb862275693c814673ab9a47e3901f9065
Jundertag <jaydenmacarthur3@gmail.com>
2025-07-25 12:09:52 -0300

    Linking Button Calls to the Foreground Service to be able to control the service

--- app/src/main/java/com/jayden/stealthService/MainActivity.kt
+++ app/src/main/java/com/jayden/stealthService/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.jayden.stealthService
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
 import com.jayden.stealthService.databinding.ActivityMainBinding
+import com.jayden.stealthService.services.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
+    private val foregroundService = ForegroundService()
+    private val serviceIntent = Intent(this, ForegroundService::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding?.buttonStartService?.setOnClickListener {
+            startForegroundService(serviceIntent)
+        }
+
+        binding?.buttonPingService?.setOnClickListener {
+            foregroundService.pingService()
+        }
+
+        binding?.buttonStopPingService?.setOnClickListener {
+            foregroundService.stopPingService()
+        }
+
+        binding?.buttonKillService?.setOnClickListener {
+            stopService(serviceIntent)
+        }
     }
 }
--- app/src/main/java/com/jayden/stealthService/services/ForegroundService.kt
+++ app/src/main/java/com/jayden/stealthService/services/ForegroundService.kt
@@ -1,4 +1,69 @@
 package com.jayden.stealthService.services
 
-class ForegroundService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.util.Log
+import com.jayden.stealthService.R
+import java.lang.Thread.sleep
+
+class ForegroundService : Service() {
+
+    var pingService = false
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+
+        private const val CHANNEL_ID = "foreground_service"
+        private const val CHANNEL_NAME = "Foreground Service"
+
+        private const val TAG = "ForegroundService"
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground(NOTIFICATION_ID, createNotification())
+        return START_STICKY
+    }
+
+    private fun createNotification(): Notification {
+        val channelId = CHANNEL_ID
+        val channelName = CHANNEL_NAME
+
+        val channel =
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+
+        return Notification.Builder(this, channelId)
+            .setContentTitle("Foreground Service")
+            .setContentText("Running in the background")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+    }
+
+    fun pingService() {
+        pingService = true
+        Log.d(TAG, "Ping Service Command Sent")
+
+        while (pingService) {
+            sleep(5000)
+            Log.i(TAG, "Ping Successful")
+        }
+    }
+
+    fun stopPingService() {
+        pingService = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        pingService = false
+    }
 }
