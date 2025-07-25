5e20be1b558f47e092aa410887374e8f038a2b43
Jundertag <jaydenmacarthur3@gmail.com>
2025-07-25 11:39:47 -0300

    Setting up MainActivity.kt to respond to lifecycle events

--- app/src/main/java/com/jayden/stealthService/MainActivity.kt
+++ app/src/main/java/com/jayden/stealthService/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.jayden.stealthService
 
 import androidx.appcompat.app.AppCompatActivity
+import com.jayden.stealthService.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
